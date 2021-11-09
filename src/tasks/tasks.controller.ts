0a6c5ab063943885d09976911a21d911aff58459
quanganh1404 <anhnq72@wru.vn>
2021-11-09 17:24:29 +0700

    CRUD

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,78 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { GetTaskFilterDto } from './dto/get-tasks-filter.dto';
+import { Task, TaskStatus } from './task.model';
+import { TasksService } from './tasks.service';
 
 @Controller('tasks')
-export class TasksController {}
+export class TasksController {
+  constructor(private tasksService: TasksService) {}
+
+  @Get()
+  // getAllTasks(): Task[] {
+  getTasks(@Query() filterDto: GetTaskFilterDto): Task[] {
+    // If we have any filter defined, call taskService.getTaskWithFilters
+    // ortherwise, just get all tasks
+    if (Object.keys(filterDto).length) {
+      // If we have any filter
+      return this.tasksService.getTaskWithFilters(filterDto);
+    } else {
+      return this.tasksService.getAllTasks();
+    }
+  }
+
+  @Post()
+  // When a request come in nestJS will takes all request body and sign in to body (red) parameters
+  createTasks(@Body() createTaskDto: CreateTaskDto): Task {
+    return this.tasksService.createTasks(createTaskDto);
+  }
+
+  @Get('/:id')
+  getTaskById(@Param('id') id: string): Task {
+    return this.tasksService.getTaskById(id);
+  }
+
+  @Delete('/:id')
+  deleteTaskById(@Param('id') id: string): void {
+    return this.tasksService.deleteTaskById(id);
+  }
+
+  @Patch('/:id/status')
+  updateTaskStatus(
+    @Param('id') id: string,
+    @Body('status') status: TaskStatus,
+  ): Task {
+    return this.tasksService.updateTaskStatus(id, status);
+  }
+
+  @Patch('/:id/description')
+  updateTaskDescription(
+    @Param('id') id: string,
+    @Body('description') description: string,
+  ): Task {
+    return this.tasksService.updateTaskDesciption(id, description);
+  }
+
+  @Patch('/:id/title')
+  updateTaskTitle(@Param('id') id: string, @Body('title') title: string): Task {
+    return this.tasksService.updateTaskTitle(id, title);
+  }
+
+  // @Post()
+  // // When a request come in nestJS will takes all request body and sign in to body (red) parameters
+  // createTasks(
+  //   @Body('title') title: string,
+  //   @Body('description') description: string,
+  // ): Task {
+  //   return this.tasksService.createTasks(title, description);
+  // }
+}
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,75 @@
 import { Injectable } from '@nestjs/common';
-
+import { Task, TaskStatus } from './task.model';
+import { v4 as uuid } from 'uuid';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { GetTaskFilterDto } from './dto/get-tasks-filter.dto';
 @Injectable()
-export class TasksService {}
+export class TasksService {
+  private tasks: Task[] = [];
+
+  getAllTasks(): Task[] {
+    return this.tasks;
+  }
+
+  getTaskWithFilters(filterDto: GetTaskFilterDto): Task[] {
+    const { status, search } = filterDto;
+    // Define a temporary array to hold the result
+    let tasks = this.getAllTasks();
+
+    // Do something with status
+    if (status) {
+      tasks = tasks.filter((task) => task.status === status);
+    }
+    // Do something with search
+    if (search) {
+      tasks = tasks.filter((task) => {
+        if (task.title.includes(search) || task.description.includes(search)) {
+          return true;
+        }
+        return false;
+      });
+      return tasks;
+    }
+    // return final result
+  }
+
+  createTasks(createTaskDto: CreateTaskDto): Task {
+    const { title, description } = createTaskDto;
+
+    const task: Task = {
+      id: uuid(),
+      title,
+      description,
+      status: TaskStatus.OPEN, //This TaskStatus takes from model
+    };
+
+    this.tasks.push(task);
+    return task;
+  }
+
+  getTaskById(id: string): Task {
+    return this.tasks.find((task) => task.id === id);
+  }
+
+  deleteTaskById(id: string): void {
+    this.tasks = this.tasks.filter((task) => task.id !== id);
+  }
+
+  updateTaskStatus(id: string, status: TaskStatus) {
+    const task = this.getTaskById(id);
+    task.status = status;
+    return task;
+  }
+
+  updateTaskDesciption(id: string, description: string) {
+    const task = this.getTaskById(id);
+    task.description = description;
+    return task;
+  }
+
+  updateTaskTitle(id: string, title: string) {
+    const task = this.getTaskById(id);
+    task.title = title;
+    return task;
+  }
+}
